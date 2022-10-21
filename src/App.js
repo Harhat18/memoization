7ea48bd98ec096ae601d18cd2acb5b946e778b15
Harun HATİBOĞLU <111196660+Harhat18@users.noreply.github.com>
2022-10-21 21:06:11 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import Header from "./components/Header";
 function App() {
   const [number, setNumber] = useState(0);
+  const [text, setText] = useState("");
+
+  const data = useMemo(() => {
+    return;
+    calculateObject();
+  }, []);
+
+  // const data = calculateObject();
   return (
     <div className="App">
-      <Header />
+      <Header data={data} />
       <hr />
       <h1>{number}</h1>
       <button onClick={() => setNumber(number + 1)}> Click</button>
+      <br /> <br />
+      <input>
+        {" "}
+        value={text} onChange={({ target }) => setText(target.value)}
+      </input>
     </div>
   );
+}
+
+function calculateObject() {
+  console.log("calculating....");
+  for (let i = 0; i < 10000000; i++) console.log("calculating complated....");
+
+  return { name: "harun" };
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,14 @@
 import React from "react";
 
-function Header() {
+function Header({ number, data }) {
   console.log("Header Component Re-rendered!");
-  return <div>Header</div>;
+  return (
+    <div>
+      Header - {number}
+      <br /> <br />
+      <code>{JSON.stringify(data)}</code>
+    </div>
+  );
 }
 
 export default React.memo(Header);
